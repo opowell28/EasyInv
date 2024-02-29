ca9c20b9756ab1cfd19f0b67b499192efe66caea
Owen Powell <opowell2899@gmail.com>
2024-02-29 15:35:43 -0500

    Enable count update on button push

--- scripts.js
+++ scripts.js
@@ -6,16 +6,22 @@ var caseButton = document.querySelector('.decrement-case');
 var bottleCount = 12;
 var caseCount = bottleCount / 12;
 
-// Select location and display inventory counts
-document.getElementById('alchemist-bottle-count').textContent = bottleCount + ' bottle';
-document.getElementById('alchemist-case-count').textContent = caseCount + ' case';
-
 // Add event listener to the bottle button
 bottleButton.addEventListener('click', function() {
+    bottleCount = bottleCount - 1;
+    // Update bottle count
+    document.getElementById('alchemist-bottle-count').textContent = bottleCount + ' bottle';
     console.log('Bottle button clicked');
 });
 
 //add event listener to the case button
 caseButton.addEventListener('click', function() {
+    caseCount = caseCount - 1;
+    // Update case count
+    document.getElementById('alchemist-case-count').textContent = caseCount + ' case';
     console.log('Case button clicked');
-})
+});
+
+// Select location and display inventory counts
+document.getElementById('alchemist-bottle-count').textContent = bottleCount + ' bottle';
+document.getElementById('alchemist-case-count').textContent = caseCount + ' case';
